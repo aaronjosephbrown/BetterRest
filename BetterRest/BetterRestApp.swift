5671a1413765861beb8681ccb3d50c2ca5cecc87
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-09-29 10:02:15 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aaron Brown on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
